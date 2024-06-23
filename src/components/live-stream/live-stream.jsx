5b8a21fac62e57b95af25f93f60bbffb17f9c503
Usman Naveed <56630061+UsmanNaveed896@users.noreply.github.com>
2024-06-23 15:51:35 +0500

    live stream

---
+++ src/components/live-stream/live-stream.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import {
+  LivestreamPlayer,
+  StreamVideo,
+  StreamVideoClient,
+  User,
+} from "@stream-io/video-react-sdk";
+
+const apiKey = "mmhfdzb5evj2";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiUHJpbmNlX1hpem9yIiwiaXNzIjoiaHR0cHM6Ly9wcm9udG8uZ2V0c3RyZWFtLmlvIiwic3ViIjoidXNlci9QcmluY2VfWGl6b3IiLCJpYXQiOjE3MTg5NDMxMDUsImV4cCI6MTcxOTU0NzkxMH0.cRHylcjf9AFonWuRA_WWOfV6dZNmofRwDyD737wrWic";
+const userId = "Prince_Xizor";
+const callId = "eLxKMCEhAxRD";
+const user: User = { id: userId, name: "Tutorial" };
+const client = new StreamVideoClient({ apiKey, user, token });
+
+export default function LiveStream() {
+  const [isLive, setIsLive] = useState(false);
+
+  useEffect(() => {
+    const checkLiveStreamStatus = async () => {
+      try {
+        const call = client.call('livestream', callId);
+        const callState = await call.get();
+        
+        const liveStartedAt = callState.call.session?.live_started_at;
+        const liveEndedAt = callState.call.session?.live_ended_at;
+        console.log(liveEndedAt,"liveEndedAt")
+
+        if (liveStartedAt && !liveEndedAt) {
+          setIsLive(true);
+        } else {
+          setIsLive(false);
+        }
+      } catch (error) {
+        console.error('Error checking livestream status:', error);
+      }
+    };
+
+    checkLiveStreamStatus();
+  }, []);
+console.log(isLive,"islive")
+  return (
+    <StreamVideo client={client}>
+      {isLive ? (
+        <LivestreamPlayer callType="livestream" callId={callId} />
+      ) : (
+        <p className='font-bold text-black'>No active livestream</p>
+      )}
+    </StreamVideo>
+  );
+}
